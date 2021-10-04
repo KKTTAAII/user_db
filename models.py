bea39d76354e7a86a00aa38487a618deab2692d6
Ktai <krataias65@gmail.com>
2021-10-03 18:47:05 -0600

    small fix

--- models.py
+++ models.py
@@ -8,11 +8,11 @@ def connect_db(app):
     db.app = app
     db.init_app(app)
 
-DEFAULT_IMAGE_URL = "https://scontent.fbkk5-5.fna.fbcdn.net/v/"
+DEFAULT_IMAGE_URL = ("https://scontent.fbkk5-5.fna.fbcdn.net/v/"
 "t31.18172-8/20746116_10154702359061231_1407828309154541812_o.jpg?_"
 "nc_cat=104&ccb=1-5&_nc_sid=84a396&_nc_ohc=PEj5I9cBUqoAX9ofBUP&_nc_oc"
 "=AQlYbIf3oF-fc7fimrwcM_Cl6QI-8JTmHGLxWqk8QwJeFViIR0AJ8-A-uYKW8fM-s8"
-"4&_nc_ht=scontent.fbkk5-5.fna&oh=47495007826807b8310ae1611e52db77&oe=618171B7"
+"4&_nc_ht=scontent.fbkk5-5.fna&oh=47495007826807b8310ae1611e52db77&oe=618171B7")
 
 class User(db.Model):
     __tablename__ = 'users'
